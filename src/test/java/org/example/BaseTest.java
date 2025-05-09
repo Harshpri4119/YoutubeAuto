4d5a2bf01fcbb0b2bed1dde51256860ddbdc0b58
HarshPri4119 <Kanakagirih@gmail.com>
2025-05-09 12:57:11 +0530

    Added Base Test

--- src/test/java/org/example/BaseTest.java
+++ src/test/java/org/example/BaseTest.java
@@ -1,8 +1,15 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.List;
 
 public class BaseTest {
 
@@ -11,9 +18,10 @@ public class BaseTest {
     String url = "https://www.youtube.com/";
     String title;
     String currentURL;
+    String searchKey = "Uyi Amma";
 
     @Test
-    public void TestLaunch() throws  Exception{
+    public void TestLaunch() throws InterruptedException {
 
         driver.get(url);
         driver.manage().window().maximize();
@@ -29,8 +37,38 @@ public class BaseTest {
         }else{
             System.out.println("URL Matched.");
         }
+
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5000));
+
+        WebElement searchElement = driver.findElement(By.xpath("//input[@name='search_query']"));
+
+
+        wait.until(ExpectedConditions.visibilityOf(searchElement));
+
+        searchElement.sendKeys(searchKey);
+
+        driver.findElement(By.xpath("//button[@title='Search']")).click();
+
         Thread.sleep(5000);
 
-        driver.quit();
+
+        List<WebElement> webElements = driver.findElements(By.xpath("//div[@id='contents']"));
+
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
+
+        for(WebElement element : webElements){
+
+            String elementText = element.getText();
+            String a = "Uyi Amma - Azaad | Aaman D, Rasha Thadani| Madhubanti Bagchi,Amit Trivedi,Amitabh| Bosco| Abhishek K";
+
+            System.out.println(elementText);
+
+            if (elementText.equals(a)){
+                driver.findElement(By.xpath("//a[@id='thumbnail']")).click();
+            }
+        }
+
     }
 }
